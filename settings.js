b6aef147614c9ee10281deb05e66063b93172931
pathum4563 <125662016+pathum4563@users.noreply.github.com>
2023-11-10 16:48:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,19 +5,19 @@ const chalk = require('chalk')
 /*
 🤖 𝗕𝗢𝗧 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OFFLINE = false // if you want put bot offline put true
-global.BOT_PRIVATE = true // if you want use bot privet 
-global.AUTO_CHATGPT = false // if you want auto ai bot mod
+global.OFFLINE = true // if you want put bot offline put true
+global.BOT_PRIVATE = false // if you want use bot privet 
+global.AUTO_CHATGPT = true // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
-global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
+global.ONLY_INBOX = true // Only inbox mod ( only pm ) use
 global.ONLY_GROUPS = false // Only Groups Use
 
 
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
+global.OWNER_NAME = 'Panda Bbh Ofc' // Owner Name
+global.OWNER_NUMBER = ['94715530122'] //Owner Number
 global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 var SESSION_ID = ''
-global.AI_PASSWORD = 'king-nima-gpt-bot'
+උglobal.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
 /*
